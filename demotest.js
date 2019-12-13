db75b9934ae098ed514d05398d609136a99b63e0
Jimmi <jimmih@gmail.com>
2019-12-13 10:32:12 -0800

    a test for the text file

---
+++ demotest.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+
+var fileName = 'test.html';
+
+var stream = fs.createReadStream(fileName);
+fs.readFile(fileName, function (err, data) {
+  if (err) throw err;
+  if(data.includes('Text')){
+   console.log('Found it')
+   process.exit()
+ } else {
+   console.log('failed')
+   process.exit(-1)
+ }
+});
